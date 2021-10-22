d1b4693ad28a6efba946bd727e30ae6b4cdbb2cc
Neelesh Garg <neelesh.garg@primathon.in>
2021-10-22 11:45:03 +0530

    nest crud with sql

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
